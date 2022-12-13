f231742845928fefc47f25e82991963454bcdf7a
Ryan peters <ryanpetersja@gmail.com>
2022-12-13 12:42:43 -0500

    removed water mark footer

--- resources/views/medquiz.blade.php
+++ resources/views/medquiz.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -9,7 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <meta http-equiv="Content-Security-Policy" content=" default-src 'self' https://medwigsjm.com" />
 
 </head>
 
@@ -114,11 +114,11 @@
                     </div>
 
                     <div class="quiz-box">
-                        {{-- <div class="row">
+                        <div class="row">
                             <div class="col-sm-4">Neem <span>@{{ neemScore }}</span></div>
                             <div class="col-sm-4">Caster <span>@{{ casterScore }}</span></div>
                             <div class="col-sm-4">Growth Blend <span>@{{ gbScore }}</span></div>
-                        </div> --}}
+                        </div>
                         <div v-for="question in questions">
                             <div v-if="question.index == currentIndex" :name="question.name" :id="question.name">
                                 <h2 class="question-title">@{{ question.question }}</h2>
